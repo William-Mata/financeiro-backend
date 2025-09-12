594e2495698c084a2d7ebc1c4d4d06cd8ae6940e
william_mata <william.xavante@gmail.com>
2025-09-12 19:08:12 -0300

    Iniciando a arquitetura do sistema

---
+++ Financeiro.Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Financeiro.Domain.Entities;
+
+public class Usuario : BaseEntity
+{
+    public string Nome { get; protected set; }
+
+    public string Email { get; protected set; }
+
+    public string Senha { get; protected set; }
+
+    public Usuario(Usuario? usuarioCadastro) : base(usuarioCadastro)
+    {
+    }
+}
